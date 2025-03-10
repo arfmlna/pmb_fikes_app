b8f331c35baa79587180a84f32d7afca55aafe0b
arfmlna <arima06917@gmail.com>
2025-03-10 21:00:59 +0700

    setup API

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -5,14 +5,14 @@ export default async function handler(req, res) {
     let date = new Date()
     let now = `${date.toISOString().split('T')[0]} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
     if (req.method === 'POST') {
-        const { name, email, password } = req.body;
+        const { name, email, password, role='users' } = req.body;
         try {
             const [existingUser] = await connect.query('SELECT * FROM users WHERE email = ?', [email]);
             if (existingUser.length > 0) {
                 return res.status(400).json({ message: 'Email sudah digunakan!' });
             }
             const hashedPassword = await bcrypt.hash(password, 10);
-            const [result] = await connect.query('INSERT INTO users (name, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?)', [name, email, hashedPassword, now, now]);    
+            const [result] = await connect.query('INSERT INTO users (name, email, password, role, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)', [name, email, hashedPassword, role, now, now]);    
             res.status(201).json({ message: 'Pengguna berhasil didaftarkan!', data: result });
         } catch (error) {
             res.status(500).json({ error: 'Terjadi kesalahan saat mendaftarkan pengguna.' });
